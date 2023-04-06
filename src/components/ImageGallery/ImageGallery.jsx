29299bfb2184c6b5feb553b38311c3f80ec48f64
SlavikChyzh <110846906+SlavikChyzh@users.noreply.github.com>
2023-04-06 20:10:27 +0300

    done
    done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -13,38 +13,18 @@ const ImageGallery = ({ searchWorld }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoadMore, setIsLoadMore] = useState(false);
   const [isModal, setIsModal] = useState('');
-
-  // const queryImages = () => {
-  //   setIsLoading(true);
-  //   setIsLoadMore(false);
-  //   getImages(searchWorld, page)
-  //     .then(response => {
-  //       if (response.length !== 0) {
-  //         setIsLoading(false);
-  //         setIsLoadMore(true);
-  //         setGallery([...gallery, ...response]);
-  //       } else if (response.length === 0) {
-  //         Report.failure('No more images to load', 'Okay');
-  //         setIsLoadMore(false);
-  //       }
-  //     })
-  //     .catch(error => {
-  //       console.log('error :>> ', error);
-  //     })
-  //     .finally(() => {
-  //       setIsLoading(false);
-  //     });
-  // };
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     setGallery([]);
+    setSearch(searchWorld);
   }, [searchWorld]);
 
   useEffect(() => {
-    if (searchWorld !== '') {
+    if (search !== '') {
       setIsLoading(true);
       setIsLoadMore(false);
-      getImages(searchWorld, page)
+      getImages(search, page)
         .then(response => {
           if (response.length !== 0) {
             setIsLoading(false);
@@ -62,7 +42,7 @@ const ImageGallery = ({ searchWorld }) => {
           setIsLoading(false);
         });
     }
-  }, [page, searchWorld]);
+  }, [page, search]);
 
   const handleModal = largeImageURL => {
     setIsModal(largeImageURL);
@@ -115,3 +95,25 @@ export { ImageGallery };
 ImageGallery.propTypes = {
   searchWorld: PropTypes.string.isRequired,
 };
+
+// const queryImages = () => {
+//   setIsLoading(true);
+//   setIsLoadMore(false);
+//   getImages(searchWorld, page)
+//     .then(response => {
+//       if (response.length !== 0) {
+//         setIsLoading(false);
+//         setIsLoadMore(true);
+//         setGallery([...gallery, ...response]);
+//       } else if (response.length === 0) {
+//         Report.failure('No more images to load', 'Okay');
+//         setIsLoadMore(false);
+//       }
+//     })
+//     .catch(error => {
+//       console.log('error :>> ', error);
+//     })
+//     .finally(() => {
+//       setIsLoading(false);
+//     });
+// };
